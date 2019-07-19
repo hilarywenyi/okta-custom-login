acfe4534c04b76d22e255f3a7ea3beea35d88bec
Tom Mao <tommao@Toms-Air.cable.rcn.com>
2019-07-19 15:27:59 -0400

    feat: front-end react part of Okta Sign-in

---
+++ src/.samples.config.js
@@ -0,0 +1,12 @@
+export default {
+    oidc: {
+      clientId: '0oawd60k6vDNoHjJi356',
+      issuer: 'https://dev-599348.okta.com/oauth2/default',
+      redirectUri: 'http://localhost:3000/implicit/callback',
+      scope: 'openid profile email',
+    },
+    resourceServer: {
+      messagesUrl: 'http://localhost:3000/api/messages',
+    },
+  };
+  
